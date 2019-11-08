401d5fbe7215599564c90ed3e48529968eb57743
Zeyu Yan <zyan01@utbm.fr>
2019-11-08 11:04:46 +0100

    ajouter login et logout

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { CounterService } from '../counter.service';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(public auth: AuthService,private breakpointObserver: BreakpointObserver,private counterservice: CounterService) {
+  }
+
+}
